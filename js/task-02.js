846b29f60d324ceab6788c920db6876c47171d2c
Fosa1990 <fosa1990@gmail.com>
2021-08-31 14:05:12 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+const ingredientsRef = document.querySelector('#ingredients');
+
+// //Mapped
+const addIngredient = (array) => array.map(element => {
+  const itemOfListEl = document.createElement('li')
+  itemOfListEl.textContent = element
+  return itemOfListEl
+});
+
+ingredientsRef.append(...addIngredient(ingredients))
+
+// //Reduced, easier like that :)
+// const reducedArrayOfIngredients = ingredients.reduce((acc, elem) =>  acc + `<li>${elem}</li>` , '')
+
+// IngredientsRef.insertAdjacentHTML('beforeend', reducedArrayOfIngredients)
