07e8abf042987f4683a84d58e3dbdaca324b3932
Fosa1990 <fosa1990@gmail.com>
2021-09-04 04:39:56 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,26 +1,5 @@
 const allCategoriesRef = document.querySelector('#categories');
 console.log(`В списке ${allCategoriesRef.children.length} категории.`);
 
-
-// forEach
 const array = [...allCategoriesRef.children]
 array.forEach(element => console.log(`Категория: ${element.firstElementChild.innerHTML} Количество элементов: ${element.lastElementChild.children.length}`));
-
-// // for...of
-// for (let child of allCategoriesRef.children) {
-//   console.log(`Категория: ${child.firstElementChild.innerHTML} Количество элементов: ${child.lastElementChild.children.length}`);
-// }
-
-// // HARDCODE XD
-// //all titles
-// const titleOfFirstSubListRef = document.querySelectorAll('.item')[0].firstElementChild.textContent;
-// const titleOfSecondSubListRef = document.querySelectorAll('.item')[1].firstElementChild.textContent;
-// const titleOfThirdSubListRef = document.querySelectorAll('.item')[2].firstElementChild.textContent;
-// // all lengths
-// const sumOfFirstSubCategoriesRef = document.querySelectorAll('.item')[0].lastElementChild.children.length;
-// const sumOfSecondSubCategoriesRef = document.querySelectorAll('.item')[1].lastElementChild.children.length;
-// const sumOfThirdSubCategoriesRef = document.querySelectorAll('.item')[2].lastElementChild.children.length;
-
-// console.log(`Категория: ${titleOfFirstSubListRef} Количество элементов: ${sumOfFirstSubCategoriesRef}`);
-// console.log(`Категория: ${titleOfSecondSubListRef} Количество элементов: ${sumOfSecondSubCategoriesRef}`);
-// console.log(`Категория: ${titleOfThirdSubListRef} Количество элементов: ${sumOfThirdSubCategoriesRef}`);
